77c66ac4632f3e29b7e0b5daa79873c9e97b3ad2
Hahinika Dilrukshi <ghashinika2002@gmail.com>
2025-02-17 10:07:52 +0530

    complain second commit

--- routes/complain.routes.js
+++ routes/complain.routes.js
@@ -7,6 +7,6 @@ const auth = require('../Middlewares/auth.middleware');
 router.post('/add-complain', auth, complainEp.createComplain);
 
 router.get('/get-complains', auth, complainEp.getComplains);
-//router.get('api/complain/reply/:id', complainEp.getComplainReplyByid );
+
 
 module.exports = router;
