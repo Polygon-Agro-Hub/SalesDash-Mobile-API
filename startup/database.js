38a26af59e7f20c9ee7f2785846cddb06f177bcd
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2025-05-05 10:25:06 +0530

    refactor database connection setup for improved clarity and maintainability

--- startup/database.js
+++ startup/database.js
@@ -1,28 +1,75 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Create a MySQL connection pool
-const createPool = (database) => {
-  return mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: database,
-    charset: 'utf8mb4',
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 6,
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0,
-  });
-};
+const plantcare = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME_PC,
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 6,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
+
+const collectionofficer = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME_CO,
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 20,
+  maxIdle: 6,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
+
+const marketPlace = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME_MP,
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 6,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
+
+const dash = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME_DS,
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 6,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
+
+const admin = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME_AD,
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 6,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
 
 
-const plantcare = createPool(process.env.DB_NAME_PC);
-const collectionofficer = createPool(process.env.DB_NAME_CO);
-const marketPlace = createPool(process.env.DB_NAME_MP);
-const dash = createPool(process.env.DB_NAME_DS);
-const admin = createPool(process.env.DB_NAME_AD);
-
-module.exports = { plantcare, collectionofficer, marketPlace, dash, admin };
+module.exports = {plantcare, collectionofficer, marketPlace, dash, admin};
